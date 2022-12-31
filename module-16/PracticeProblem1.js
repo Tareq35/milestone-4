ab2b237cf6e66c94042a2c863fa968a951570bd8
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-01 00:09:12 +0600

    first commit

---
+++ module-16/PracticeProblem1.js
@@ -0,0 +1,9 @@
+// var momGave= 1000;
+// var applePrice= 300;
+// var orangePrice= 400;
+// var totalPrice = (applePrice+orangePrice);
+
+// var returnedMoney = (momGave-totalPrice);
+
+// console.log(returnedMoney);
+
