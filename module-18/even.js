79874418e691842837733519dfafd4fc71168cdc
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-01 17:12:52 +0600

    updated

---
+++ module-18/even.js
@@ -0,0 +1,11 @@
+var number = 0;
+while(number <= 10){
+    console.log(number);
+    number= number+2;
+}
+// odd numbers
+var number = 1;
+while(number <= 10){
+    console.log(number);
+    number= number+2;
+}
---
+++ module-18/for.js
@@ -0,0 +1,15 @@
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+    console.log('Roast Den, Please');
+}
+
+for (var i = 0; i < 7; i++) {
+    console.log(i);
+}
+// odd number
+for (var i = 1; i < 20; i += 2) {
+    console.log(i);
+}
+//even number
+for (var i = 0; i < 20; i += 2) {
+    console.log(i);
+}
