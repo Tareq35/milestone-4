b16613709016cfd0f42b01b0ef9a604f19ee5163
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-02 21:34:58 +0600

    updated

---
+++ module-19/function.js
@@ -0,0 +1,15 @@
+function startFan() {
+    console.log("stand up");
+    console.log('walk towards the switch');
+    console.log('press the button');
+}
+// call the function
+startFan();
+console.log('waking up in the  morning');
+startFan();
+console.log('eating breakfast');
+startFan();
+console.log('eating lunch');
+startFan();
+console.log('play with friends');
+startFan();
---
+++ module-19/object.js
@@ -0,0 +1,8 @@
+var myComputer = {
+    brand: 'lenovo',
+    color: 'silver',
+    price: 39000,
+    processor: 'i7'
+}
+
+console.log(myComputer)
