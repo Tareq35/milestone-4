b76520c97d0880d516d56c9d50b0371190c1f162
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-03 16:12:07 +0600

    updated

---
+++ module-20/inchToFeet.js
@@ -0,0 +1,11 @@
+const myInches= 12;
+const myFeet= myInches/12;
+console.log(myFeet);
+
+const dadaInches= 144;
+const dadaFeet= dadaInches/12;
+console.log('dadar height',dadaFeet, 'feet');
+
+const dadiInches= 60;
+const dadiFeet= dadiInches/12;
+console.log('dadir height',dadiFeet, 'feet');
