1d25e641d0ffc9d1c5a6940c1c837ea8284b0ae5
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-03 18:55:07 +0600

    updated

--- module-20/inchToFeet.js
+++ module-20/inchToFeet.js
@@ -1,11 +1,24 @@
-const myInches= 12;
-const myFeet= myInches/12;
-console.log(myFeet);
+// const myInches= 12;
+// const myFeet= myInches/12;
+// console.log(myFeet);
 
-const dadaInches= 144;
-const dadaFeet= dadaInches/12;
-console.log('dadar height',dadaFeet, 'feet');
+// const dadaInches= 144;
+// const dadaFeet= dadaInches/12;
+// console.log('dadar height',dadaFeet, 'feet');
 
-const dadiInches= 60;
-const dadiFeet= dadiInches/12;
-console.log('dadir height',dadiFeet, 'feet');
+// const dadiInches= 60;
+// const dadiFeet= dadiInches/12;
+// console.log('dadir height',dadiFeet, 'feet');
+
+function inchToFeet(inches){
+    const feet = inches / 12;
+    return feet;
+}
+
+const dadaInches = 144;
+const dadaFeet = inchToFeet(dadaInches);
+console.log(dadaFeet);
+
+const nanaInches = 144;
+const nanaFeet = inchToFeet(nanaInches);
+console.log('nanar height',nanaFeet, 'feet');
