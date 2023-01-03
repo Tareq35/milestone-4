e588bd96ba1b306b11620f8b937f62a6256b1265
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-04 00:13:29 +0600

    updated

---
+++ module-20/isLeapYear.js
@@ -0,0 +1,12 @@
+function isLeapYear(year) {
+    const reminder = year % 4;
+    if (reminder === 0) {
+        return true;
+    }
+    else {
+        return true;
+    }
+}
+const isThisYearLeapYear = isLeapYear(2024);
+
+console.log(isThisYearLeapYear);
