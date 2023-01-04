aef6f3a62ac3a56efd36e930edbb0fbe12188ed5
Tareq35 <a.t.tariqulislam@gmail.com>
2023-01-04 19:21:19 +0600

    updated

---
+++ module-21/split-join.js
@@ -0,0 +1,8 @@
+const lyrics= 'Tumi bondhu kala pakhi ami jeno ki. boshonto kale tomay bolte pari ni. kala kala sada sada rong jomeche sada kalay.';
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const character= lyrics.split('');
+// console.log(character);
+
+const partial = lyrics.slice(5, 12);
+console.log(partial);
